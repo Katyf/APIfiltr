a35694f1c83eabbe00ae1ccda72cc3bde8785da5
Katy Feng <katy.feng.14@gmail.com>
2015-03-12 16:36:21 -0400

    Revert "delete images and votes models and add them as properties of post instea..."

--- db/migrate/20150312134005_create_posts.rb
+++ db/migrate/20150312134005_create_posts.rb
@@ -2,10 +2,6 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.string :message
-      t.string :img1
-      t.string :img2
-      t.integer :vote1
-      t.integer :vote2
 
       t.timestamps
     end
