c97d0d6a23365699922801008029e995affafb7a
Gina Villarreal <gina.nvillarreal@gmail.com>
2015-03-12 11:26:28 -0400

    added middleware to application.rb

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,12 @@ Bundler.require(*Rails.groups)
 
 module FiltrAPI
   class Application < Rails::Application
+    config.middleware.use Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options]
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
