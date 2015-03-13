0907a7e3ee3c33797067089f206636fcf2c5307d
J-Weeks <weeksjasons@gmail.com>
2015-03-13 09:35:19 -0400

    added and refactored comments routing spec. show and create pass

--- spec/routing/comments_routing_spec.rb
+++ spec/routing/comments_routing_spec.rb
@@ -2,26 +2,26 @@ require 'rails_helper'
 
 
 RSpec.describe "routes for commenets" do
-    before(:all) do
-    Comment.destroy_all
-    Post.destroy_all
-    FactoryGirl.create_list(:post, 2)
-    FactoryGirl.create_list(:comment, 2)
+  it 'routes POST /posts/:post_id/comments to the posts-comments controller' do
+    expect(post("/posts/1/comments")).to route_to({
+      controller: 'comments',
+      action: 'create',
+      post_id: '1'
+    })
   end
 
-  # it 'routes POST /posts/id/comments to the posts-comments controller' do
-  #   @post = Post.first
-  #   @comment = Comment.first
-  #   byebug
-  #   expect(post("/posts/#{@post.id}/comments")).to route_to('comments#create')
-  # end
-
-  it 'routes GET /posts/id to the posts controller' do
-  post = FactoryGirl.create(:post)
-  expect(get("/posts/#{post.id}")).to route_to('posts#show', id: post.id.to_s)
+  it 'routes GET /posts/:post_id/comments/:id to the comments controller' do
+    expect(get("/posts/1/comments/2")).to route_to({
+      controller: 'comments',
+      action: 'show',
+      post_id: '1',
+      id: '2'
+    })
   end
 end
 
 
 
-#post_comments POST /posts/:post_id/comments(.:format)          comments#create
+
+
+# post_comment GET  /posts/:post_id/comments/:id(.:format)      comments#show
